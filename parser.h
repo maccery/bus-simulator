14851767caa7c4d1123c95b13a6e4ad37e83705b
Tom Macmichael <tommacmichael@me.com>
2015-11-19 17:24:07 +0000

    Parser is now a separate module, called on from the main file

---
+++ parser.h
@@ -0,0 +1,37 @@
+//
+// Created by Tom Macmichael on 19/11/2015.
+//
+
+#ifndef CSLP_PARSER_H
+#define CSLP_PARSER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
+
+#define MATRIX_WIDTH 12
+#define MATRIX_HEIGHT 8
+
+struct ParsedFile {
+    int busCapacity;
+    int boardingTime;
+    float requestRate;
+    float pickupInterval;
+    int maxDelay;
+    int noBuses;
+    int noStops;
+    int stopTime;
+    int map[MATRIX_HEIGHT][MATRIX_WIDTH]; // Max size of the map is 6x6
+};
+
+struct ParsedFile *ParsedFile_create(int busCapacity, int boardingTime, float requestRate, float pickupInterval,
+                                     int maxDelay, int noBuses, int noStops, int stopTime);
+void ParsedFile_destroy(struct ParsedFile *file);
+void ParsedFile_print(struct ParsedFile *file);
+void ParsedFile_mapPrint(struct ParsedFile *file);
+void printParsedFile(struct ParsedFile *parsedFile);
+void parseFile(FILE *file);
+
+#endif //CSLP_PARSER_H
